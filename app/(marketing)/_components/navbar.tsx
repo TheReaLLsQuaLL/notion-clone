c11fa82be63e630416397e316d2c5e468b438f32
TheReaLLsQuaLL <toprakyaramaz@hotmail.com>
2023-10-26 18:21:04 +0300

    -feat- Made the authentication screen come after click on button added useConvexAuth

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import useScrollTop from "@/hooks/use-scroll-top";
+import { SignInButton } from "@clerk/nextjs";
+import { useConvexAuth } from "convex/react";
 import { cn } from "@/lib/utils";
 import React from "react";
 import Logo from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Navbar() {
   const scrolled = useScrollTop();
-
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div
       className={cn(
@@ -18,6 +21,19 @@ export default function Navbar() {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <p>Loading...</p>}
+        {!isAuthenticated && !isAuthenticated && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Notion-Clone Free</Button>
+            </SignInButton>
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
