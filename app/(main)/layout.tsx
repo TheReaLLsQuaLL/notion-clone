76f2e6a46fde1477188dc1c0964579ea180f8728
TheReaLLsQuaLL <toprakyaramaz@hotmail.com>
2023-10-27 13:12:37 +0300

    -feat-Started the sidebar using useStates and Query hooks to collapse sidebar work on progress on sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,6 +2,7 @@
 import Spinner from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
 
 export default function MainLayout({
   children,
@@ -22,5 +23,10 @@ export default function MainLayout({
     return redirect("/");
   }
 
-  return <div>{children}</div>;
+  return (
+    <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">{children} </main>
+    </div>
+  );
 }
